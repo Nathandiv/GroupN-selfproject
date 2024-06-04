18bbeb621b8def3ff4abedf4c187548017faee4d
unknown <nathan.hlungwani123@gmail.com>
2024-06-04 20:14:18 +0200

    added if statement

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ let Employee = prompt('Enter Employee Name');
 let department = prompt('Enter Group Name');
 let Gross = Number (prompt('Enter your gross income'));
 let P = 0.2
+if (department === 'D7'){
+    alert ( Employee + " You Net Income is"  + " " + (Gross + 550 - (Gross * P)));
+    }
